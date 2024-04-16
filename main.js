796cb80ac9c7884a7325eb8f7986be4d0b92dab0
Hashir Akbar <hashirbusinessinfo@gmail.com>
2024-04-16 03:24:24 -0700

    Revert "navbar update"
    This reverts commit b19dbecdb7331eb17a3a954a11ed66d360340867.

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
 import "./style.css";
-
-const navbarMobile = document.getElementById('navbar-toggle-btn')
-const navbarMobileContent = document.getElementById('navbar-search')
-
-navbarMobile.addEventListener("click",()=>{
-    navbarMobileContent.classList.toggle('hidden')
-})

bface6094cfe3839b94fe3c7b5540b390fa21b56
Waqar Ahmed <waqarhafeez12@gmail.com>
2024-04-16 12:47:59 +0500

    Added navbar:

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./pages/*.html", "./*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
